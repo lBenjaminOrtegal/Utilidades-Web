f6daa966c8ce5cfc16a82f77421c7a8a3f788089
Benjamin Desktop <benjamin.ortega.quinteros@gmail.com>
2025-10-15 00:38:15 -0300

    ahora si

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'https://lbenjaminortegal.github.io/Utilidades-Web/',
+  base: '/Utilidades-Web/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // ESTA LÍNEA ES LA CLAVE
